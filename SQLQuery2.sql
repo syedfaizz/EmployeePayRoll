ef9d2aacde230143c5e88bab579c7755fd9cf146
Sayed Faisal <86817472+syedfaizz@users.noreply.github.com>
2021-08-27 17:30:09 +0530

    Update SQLQuery2.sql

--- SQLQuery2.sql
+++ SQLQuery2.sql
@@ -1,5 +1,5 @@
 create database employeepayroll1
-create table employeepayroll(
+create table employeepayroll1(
 
 e_id int not null,
 e_name varchar(20),
@@ -25,6 +25,65 @@ insert into employeepayroll values(
 insert into employeepayroll values(
 4,'Julin',55000,32,'Male','28-06-2020'
 );
+
+select * from employeepayroll
+
+select e_name from employeepayroll;
+
+select e_name,e_salary,e_gender from employeepayroll;
+
+select distinct e_gender from employee;
+
+select * from employee where e_age<30;
+
+
+select * from employeepayroll where e_name = 'Anne'
+
+update employeepayroll 
+set e_salary = 90000
+where e_id = 1
+
+update employeepayroll
+ select e_salary from employeepayroll where e_id=2
+ select e_salary from employeepayroll where e_name = 'Anne'
+
+ select * from employeepayroll
+ where e_name = 'Anne' and e_starttime between cast('25-6-2020' as date) and GETDATE()
+
+
+
+ select SUM(e_salary) from employeepayroll
+ where e_gender='Male' Group by e_gender
+
+ select SUM(e_salary) from employeepayroll
+ where e_gender='Female' Group by e_gender
+
+ select AVG(e_salary) from employeepayroll
+ where e_gender='Male' Group by e_gender
+
+
+ select AVG(e_salary) from employeepayroll
+ where e_gender='Female' Group by e_gender
+
+ select MIN(e_salary) from employeepayroll
+ where e_gender='Male' Group by e_gender
+
+ select MIN(e_salary) from employeepayroll
+ where e_gender='Female' Group by e_gender
+
+
+ select MAX(e_salary) from employeepayroll
+ where e_gender='Male' Group by e_gender
+
+
+ select MAX(e_salary) from employeepayroll
+ where e_gender='Female' Group by e_gender
+
+ select COUNT(e_salary) from employeepayroll
+ where e_gender='Male' Group by e_gender
+
+ select COUNT(e_salary) from employeepayroll
+ where e_gender='Female' Group by e_gender
 
 
 
