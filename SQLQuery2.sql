7025f8bc041686adedaa831058a5ebc7550ec3bd
Sayed Faisal <86817472+syedfaizz@users.noreply.github.com>
2021-08-27 09:32:51 +0530

    Update SQLQuery2.sql

--- SQLQuery2.sql
+++ SQLQuery2.sql
@@ -1,42 +1,31 @@
-create database employeepayroll
-create table employee(
+create database employeepayroll1
+create table employeepayroll(
 
 e_id int not null,
 e_name varchar(20),
 e_salary int,
 e_age int,
 e_gender varchar(20),
-e_dept varchar(20),
+e_starttime varchar(20),
 primary key(e_id)
 );
 
-insert into employee values(
-1,'Sam',95000,45,'Male','Operations'
+insert into employeepayroll values(
+1,'Sam',95000,45,'Male','25-06-2020'
 );
-insert into employee values(
-2,'BOb',80000,21,'Male','support'
+
+insert into employeepayroll values(
+2,'BOb',80000,21,'Male','26-06-2020'
 );
-insert into employee values(
-3,'Anne',90000,25,'Female','Analytics'
+
+insert into employeepayroll values(
+3,'Anne',90000,25,'Female','27-06-2020'
 ); 
-insert into employee values(
-4,'Julin',55000,32,'Male','Operations'
+
+insert into employeepayroll values(
+4,'Julin',55000,32,'Male','28-06-2020'
 );
 
-select e_name from employee;
-
-select e_name,e_salary,e_gender from employee;
- 
-select * from employee
-
-select distinct e_gender from employee;
-
-select * from employee where e_age<30;
-
-select * from employee where e_dept='Operatons' OR e_dept='Analytics';
-
-
-select * from employee where e_dept='Operatons' AND e_salary>80000;
 
 
 
